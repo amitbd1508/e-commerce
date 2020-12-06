d5381c5840e4b61acbe04069fd3ae8ce0879ccbe
Amit <amit.ghosh@jeeon.co>
2020-12-07 01:46:39 +0600

    Refactor User api test code Add test code for user api

--- server/test/users.spec.ts
+++ server/test/users.spec.ts
@@ -1,37 +1,58 @@
 import * as chai from 'chai';
 import chaiHttp = require('chai-http');
-import { describe, it } from 'mocha';
+import { describe, it, before, after } from 'mocha';
 
-process.env.NODE_ENV = 'test';
 import { app } from '../app';
 import User from '../models/user';
-import * as jwt from 'jsonwebtoken';
+
+process.env.NODE_ENV = 'test';
 
 chai.use(chaiHttp).should();
 
-function generateAccessToken(user): any {
-  return jwt.sign({
-    _id: user._id,
-    name: user.name,
-    email: user.email
-  }, process.env.SECRET_TOKEN, {
-    expiresIn: '7d',
-    issuer: user._id.toString()
-  });
-}
-describe('Users', () => {
+const testUser1 = {name: 'Amit Ghosh', email: 'test@test.com', password: 'strong_password'};
+const testUser2 = {name: 'Amit Ghosh', email: 'test2@test.com', password: 'strong_password'};
 
-  beforeEach(done => {
+describe('User API', () => {
+
+  let token = '';
+
+  before(() => {
+    // Register a user for getting token
+    chai.request(app)
+      .post('/api/register')
+      .send(testUser1)
+      .end((err, res) => {
+        token = res.body.token;
+      });
+  });
+
+  after((done) => {
+    // Remove all user test data for next use
     User.remove({}, err => {
       done();
     });
   });
 
+
   describe('Backend tests for users', () => {
     it('should get register a new user', done => {
       chai.request(app)
         .post('/api/register')
-        .send({name: 'Amit Test', email: 'testuser@test.com', password: 'amit123456'})
+        .send(testUser2)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.a.property('id');
+          res.body.should.have.a.property('email');
+          res.body.should.have.a.property('token');
+          done();
+        });
+    });
+
+    it('should get log in a user', done => {
+      chai.request(app)
+        .post('/api/login')
+        .send({email: 'test@test.com', password: 'strong_password'})
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
@@ -45,9 +66,20 @@ describe('Users', () => {
     it('should not get log in a user', done => {
       chai.request(app)
         .post('/api/login')
-        .send({email: 'user@test.com', password: 'amit123456'})
+        .send({email: 'test@test.com', password: 'wrong password'})
         .end((err, res) => {
           res.should.have.status(401);
+          done();
+        });
+    });
+
+    it('should get current account', done => {
+      console.log(token);
+      chai.request(app)
+        .get('/api/account')
+        .set({Authorization: `Bearer ${token}`})
+        .end((err, res) => {
+          res.should.have.status(200);
           done();
         });
     });
@@ -61,7 +93,6 @@ describe('Users', () => {
         });
     });
   });
-
 });
 
 
