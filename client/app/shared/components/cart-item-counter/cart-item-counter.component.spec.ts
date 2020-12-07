32f18feda450eb1c5bc945b5e9e1ab815b32c918
Amit <amit.ghosh@jeeon.co>
2020-12-08 00:32:34 +0600

    Add cart-item-counter.component test code

--- client/app/shared/components/cart-item-counter/cart-item-counter.component.spec.ts
+++ client/app/shared/components/cart-item-counter/cart-item-counter.component.spec.ts
@@ -1,7 +1,15 @@
-/*
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {CartItemCounterComponent} from './cart-item-counter.component';
+import { CartItemCounterComponent } from './cart-item-counter.component';
+import { CartService } from '../../../shopping-cart/cart.service';
+
+
+class CartServiceMock {
+  getCartItemsCount(): number {
+    return 0;
+  }
+
+}
 
 describe('CartItemCounterComponent', () => {
   let component: CartItemCounterComponent;
@@ -9,10 +17,14 @@ describe('CartItemCounterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CartItemCounterComponent]
+      declarations: [CartItemCounterComponent],
+      providers: [
+        { provide: CartService, useClass: CartServiceMock }
+      ]
     })
       .compileComponents();
   });
+
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CartItemCounterComponent);
@@ -24,4 +36,3 @@ describe('CartItemCounterComponent', () => {
     expect(component).toBeTruthy();
   });
 });
-*/
